2c10ae721602a75c898fb8491c971a740c2688b0
PX Slip <408277+pxslip@users.noreply.github.com>
2021-02-07 00:59:17 -0500

    So using data provided, generate insert commands

---
+++ generate-inserts.js
@@ -0,0 +1,23 @@
+// This script is used as part of the pipeline (or manually) to update the insertion commands needed to seed the DB
+const join = require('path').join;
+const {open, writeFile} = require('fs/promises');
+
+
+(async function () {
+  const handle = await open(join(__dirname, 'tags.csv'));
+  const crafts = (await handle.readFile({encoding: 'utf-8'})).split('\n');
+  for (const line of crafts) {
+    let i = 1;
+    while (crafts.length > 0) {
+      const craftSlice = crafts.splice(0, 10);
+      const craftSdict = craftSlice.join(' ').split('","').join('" "');
+      const tpl = `{{range $shortcut, $desc := sdict ${craftSdict}}}
+{{dbSet 0 (joinStr "" "poehcs_craft_" $shortcut) $desc}}
+{{end}}
+Inserted Craft Group ${i}`;
+      writeFile(join(__dirname, `insert-crafts-${i}.go.tmpl`), tpl);
+      i++;
+    }
+  }
+  handle.close();
+})()
